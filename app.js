cccb4defe3f280ca413d17f02a14f8ff79c2395a
shuttlesaifdev <saiful@shuttlebd.com>
2019-07-15 16:17:13 +0600

    added morgan and nodemon

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
 const productRoutes = require('./api/routes/product');
 const orderRoutes = require('./api/routes/order');
 
+app.use(morgan('dev'));
 
 app.use('/products',productRoutes);
 app.use('/orders',orderRoutes);
